57f2961b5a901c6139df2d53fbbd74be69d34675
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-08-01 14:26:59 +0100

    Added KittensController show action and view.

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -2,4 +2,14 @@ class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
   end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :softness, :cuteness)
+  end
 end
