4ef234836908d4e36f779aeaf88010c49f7efa28
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-08-01 12:55:36 +0100

    Set default root to kittens#index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL avail able within this file, see https://guides.rubyonrails.org/routing.html
   resources :kittens
+  root 'kittens#index'
 end
