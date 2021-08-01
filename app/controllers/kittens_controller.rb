de1e425078c6804d3cc82c59dac3dbe6b4f9ae35
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-08-01 16:02:54 +0100

    Added new and create actions and views to KittensController.

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -7,6 +7,22 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
   end
 
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = "Kitten successfully created"
+      redirect_to @kitten
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+
   private
 
   def kitten_params
