66c20cccce351e68162596d70a1de73be5cba874
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-08-01 13:50:02 +0100

    Added Guard and LiveReload gems and config.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     # config.middleware.insert_before Rack::Lock, Rack::LiveReload
 
     # or, if you're using better_errors with rails 6.
-    config.middleware.insert_before Rack::Runtime, Rack::LiveReload
+    # config.middleware.insert_before Rack::Runtime, Rack::LiveReload
 
     # ...
   end
